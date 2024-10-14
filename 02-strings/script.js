35cac75bea5dbdba9743ad8b5d1db7086bfe7fae
klausqama <klaussqama@gmail.com>
2024-10-14 15:42:59 +0200

    Add ex 02.string

--- 02-strings/script.js
+++ 02-strings/script.js
@@ -8,8 +8,12 @@ console.log(name)
 // 2. Tipi primitivi
 // Crea tre variabili chiamate number, text e bool e assegna rispettivamente
 // un numero, una stringa e un valore booleano.
-// Stampa tutti i valori nella console.
 
+const number = 10
+const text = 'ciao sono klaus'
+const bool = true
+// Stampa tutti i valori nella console.
+console.log(number,text,bool)
 // 3. Sostituisci null nelle righe sottostanti utilizzando operazioni con le stringhe
 // e le variabili sopra in modo da stampare correttamente 
 // in console la stringa con il nome completo
@@ -17,10 +21,10 @@ const alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'; // NON MODIFICARE QUESTA RIGA
 const firstName = 'Jane'; // NON MODIFICARE QUESTA RIGA
 const lastName = 'Smith'; // NON MODIFICARE QUESTA RIGA
 
-const fullName = null;
+const fullName = `${firstName} ${lastName}`;
 console.log(fullName); // Output: "Jane Smith"
 
 // 4. Imposta questa variabile utilizzando una proprietà
 // sulla variabile alphabet per ottenere la sua lunghezza
-const numberOfLettersInAlphabet = null;
+const numberOfLettersInAlphabet = alphabet.length;
 console.log(numberOfLettersInAlphabet); // Output: 26 
